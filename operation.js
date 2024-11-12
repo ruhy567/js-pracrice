daf36d86628192876d93ce1c17f44a1b7581e097
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-12 20:38:05 +0600

    all

---
+++ operation.js
@@ -0,0 +1,21 @@
+var onionprice = 10;
+var eggprice = 30;
+
+console.log(eggprice - onionprice);
+console.log(onionprice + eggprice);
+console.log(eggprice * onionprice);
+console.log(eggprice / onionprice);
+
+console.log(10/3);
+console.log(10%3);
+console.log(11%7);
+
+var price =35;
+//price = price + 10;
+//console.log(price);
+
+var newPrice = price + 50;
+var discountPrice = newPrice - 10;
+console.log(discountPrice);
+price +=10;
+console.log(price);
