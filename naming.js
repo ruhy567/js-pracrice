77755e6675e2e880213c99e35abdcc2914fd1821
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-12 20:17:03 +0600

    all

--- naming.js
+++ naming.js
@@ -1,11 +1,19 @@
 var orangeprice = 20;
 var chocolate = 0.45;
-var appleprice = parseInt("22.5");
-
+//var appleprice = parseInt("22.5");
+var appleprice = parseFloat('22.5');
 console.log (appleprice + orangeprice);
 console.log ( orangeprice);
-console.log ( orangeprice);
+console.log ( appleprice);
 
 //variable practicing
 const jahid = "ruhy";
 console.log(jahid);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total.toFixed(2));
+
+const num = 50 / 0;
+console.log(num);
